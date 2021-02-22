48ff73b5cb54c7ac648d3c8349e69119d85c34b7
Paul Adamson <paul.adamson.01@gmail.com>
2021-02-22 07:09:58 -0500

    add functional test for independent webinars

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -28,7 +28,7 @@ class NewVisitorTest(LiveServerTestCase):
                 time.sleep(0.5)
 
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_webinar_for_one_user(self):
         # Kirsten has heard of a cool new online app for managing VLS workflow.
         # She goes to check out its homepage.
         self.browser.get(self.live_server_url)
@@ -64,11 +64,46 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_in_list_table('1: Schedule Zoom meeting')
         self.wait_for_row_in_list_table('2: Schedule invitation email campaign')
         
-        # Kirsten wonders whether the site will remember her list. Then
-        # she sees that the site has generated a unique URL for her -- there
-        # is some explanatory text to that effect.
-        self.fail('Finish the test!')        
-        
-        # She visits that URL - her to-do list is still there.
-        
         # Satisfied, she closes the web browser.
+
+    def test_multiple_users_can_start_webinar_at_different_urls(self):
+        # Kirsten starts a new webinar to-do list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Schedule April Zoom meeting')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Schedule April Zoom meeting')
+
+        # She notices that the site has generated a unique URL for her
+        kirsten_list_url = self.browser.current_url
+        self.assertRegex(kirsten_list_url, '/webinar/.+')
+        
+        # Now a new user, Francis, comes along to the site.
+        ## We use a new browser session to make sure that no information
+        ## of Kirsten's is coming through from cookies etc
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+    
+        # Aaron visits the home page.  There is no sign of Kirsten's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Schedule April Zoom meeting', page_text)
+    
+        # Aaron starts a new list by entering a new item.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Schedule May Zoom meeting')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Schedule May Zoom meeting')
+    
+        # Aaron gets his own unique URL
+        aaron_list_url = self.browser.current_url
+        self.assertRegex(aaron_list_url, '/lists/.+')
+        self.assertNotEqual(aaron_list_url, kirsten_list_url)
+    
+        # Again, there is no trace of Kirsten's list, and Aaron's is there
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Schedule April Zoom meeting', page_text)
+        self.assertIn('Schedule May Zoom meeting', page_text)
+    
+        # Satisfied, they both close their browsers
+        self.fail('Finish the test!')        
