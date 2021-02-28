c8496fbf840e4ab6bda781b8d25ebcb7298ea95b
Paul Adamson <paul.adamson.01@gmail.com>
2021-02-28 15:14:43 -0500

    change names in FT, update todo list

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # TODO
 
+- adjust model so that items are associated with different lists
 - separate lists for each webinar
 - unique URL for each webinar
 - URL for creating a new list via POST
 - URL for adding a new item to an existing list via POST
-- autogenerate webinar to-do lists
+- auto-generate webinar to-do lists
--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -28,8 +28,8 @@ class NewVisitorTest(LiveServerTestCase):
                 time.sleep(0.5)
 
 
-    def test_can_start_a_webinar_for_one_user(self):
-        # Kirsten has heard of a cool new online app for managing VLS workflow.
+    def test_can_start_a_list_for_one_webinar(self):
+        # Edith has heard of a cool new online app for managing VLS workflow.
         # She goes to check out its homepage.
         self.browser.get(self.live_server_url)
 
@@ -60,14 +60,14 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys('Schedule invitation email campaign')
         inputbox.send_keys(Keys.ENTER)
 
-        # The page updates again, and now shows both items on her list
-        self.wait_for_row_in_list_table('1: Schedule Zoom meeting')
+        # The page updates again, and now shows both items on the webinar list
         self.wait_for_row_in_list_table('2: Schedule invitation email campaign')
+        self.wait_for_row_in_list_table('1: Schedule Zoom meeting')
         
         # Satisfied, she closes the web browser.
 
-    def test_multiple_users_can_start_webinar_at_different_urls(self):
-        # Kirsten starts a new webinar to-do list
+    def test_multiple_users_can_start_webinar_list_at_different_urls(self):
+        # Edith starts a new webinar to-do list
         self.browser.get(self.live_server_url)
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Schedule April Zoom meeting')
@@ -75,32 +75,33 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_in_list_table('1: Schedule April Zoom meeting')
 
         # She notices that the site has generated a unique URL for her
-        kirsten_list_url = self.browser.current_url
-        self.assertRegex(kirsten_list_url, '/webinars/.+')
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/webinars/.+')
         
         # Now a new user, Francis, comes along to the site.
         ## We use a new browser session to make sure that no information
-        ## of Kirsten's is coming through from cookies etc
+        ## of Edith's is coming through from cookies etc
         self.browser.quit()
         self.browser = webdriver.Firefox()
     
-        # Aaron visits the home page.  There is no sign of Kirsten's list
+        # Francis visits the home page.  There is no sign of Edith's list
         self.browser.get(self.live_server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Schedule April Zoom meeting', page_text)
+        self.assertNotIn('Schedule invitation email campaign', page_text)
     
-        # Aaron starts a new list by entering a new item.
+        # Francis starts a new list by entering a new item.
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Schedule May Zoom meeting')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Schedule May Zoom meeting')
     
-        # Aaron gets his own unique URL
-        aaron_list_url = self.browser.current_url
-        self.assertRegex(aaron_list_url, '/lists/.+')
-        self.assertNotEqual(aaron_list_url, kirsten_list_url)
+        # Francis gets his own unique URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/webinars/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
     
-        # Again, there is no trace of Kirsten's list, and Aaron's is there
+        # Again, there is no trace of Edith's list, and Francis's is there
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Schedule April Zoom meeting', page_text)
         self.assertIn('Schedule May Zoom meeting', page_text)
